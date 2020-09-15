285a77dc14ca2a7b37c5472e48f7ff998ea7d9c3
Meghana Manjunatha <mmanjunatha@teksystems.com>
2020-09-15 11:11:59 -0700

    UI components added

---
+++ WebApp/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { DescriptionComponent } from './description/description.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    DescriptionComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

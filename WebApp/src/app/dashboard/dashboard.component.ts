9faa6e035dbea04f9e9e69618fc73296db0eaade
Meghana Manjunatha <mmanjunatha@teksystems.com>
2020-09-16 10:48:49 -0700

    icon added

--- WebApp/src/app/dashboard/dashboard.component.ts
+++ WebApp/src/app/dashboard/dashboard.component.ts
@@ -14,90 +14,96 @@ export class DashboardComponent implements OnInit {
   ngOnInit(): void {
     this.data = [
       {
-      date: 'Jul 22',
-      list_name: 'Megna',
+      date: '',
+      list_name: 'Competitive Intelligence',
       entities: 0,
       description: [
-        'company 1', 'company 1', 'company 1', 'company 1', 'company 1', 'company 1', 'company 1', 'company 1', 'company 1', 'company 1',
-        'company 1', 'company 1', 'company 1', 'company 1', 'company 1', 'company 1', 'company 1', 'company 1', 'company 1', 'company 1',
-        'company 1', 'company 1', 'company 1', 'company 1', 'company 1', 'company 1', 'company 1', 'company 1', 'company 1', 'company 1',
-        'company 1', 'company 1', 'company 1', 'company 1', 'company 1', 'company 1', 'company 1', 'company 1', 'company 1', 'company 1'
+        'Bundle Technology Private Limited', 'Hector Bevarages Private Limited', 'Puma Sports India Private Limited',
+        'Dewan Housing Finance Corporation Ltd.', 'Infosys Limited', 'Think and Learn Private Limited', 'Delhivery Private Limited',
+        'Wow Momo Foods Private Limited', 'Rebel Foods Private Limited', 'Lendingkart Technologies Private Limited',
+        'ID Fresh food private limited', 'Bharatiaasha Private Limited', 'Gala Precision Engineering Private Limited',
+        'Balaji Wafers Private Limited', 'Sangeeta Mobiles Private Limited', 'Vodafone Idea Limited', 'Vakrangee Limited',
+        'Bundle Technology Private Limited', 'Hector Bevarages Private Limited', 'Puma Sports India Private Limited',
+        'Dewan Housing Finance Corporation Ltd.', 'Infosys Limited', 'Think and Learn Private Limited', 'Delhivery Private Limited',
+        'Wow Momo Foods Private Limited', 'Rebel Foods Private Limited', 'Lendingkart Technologies Private Limited',
+        'ID Fresh food private limited', 'Bharatiaasha Private Limited', 'Gala Precision Engineering Private Limited',
+        'Balaji Wafers Private Limited', 'Sangeeta Mobiles Private Limited', 'Vodafone Idea Limited', 'Vakrangee Limited'
       ]
      },
      {
-      date: 'Jul 22',
-      list_name: 'Test1',
-      entities: 1,
+      date: '',
+      list_name: 'My Vendors',
+      entities: 0,
       description: [
         'company 2', 'company 1', 'company 1', 'company 1', 'company 1'
       ]
      },
      {
-      date: 'Jul 22',
-      list_name: 'Test12',
+      date: '',
+      list_name: 'My Customers',
       entities: 0,
       description: [
         'company 3', 'company 1', 'company 1', 'company 1', 'company 1'
       ]
      },
      {
-      date: 'Jul 22',
-      list_name: 'Vendor',
-      entities: 90,
+      date: 'Jul 23',
+      list_name: 'Test_30_results',
+      entities: 28,
       description: [
         'company 4', 'company 1', 'company 1', 'company 1', 'company 1'
       ]
      },
      {
-      date: 'Jul 22',
-      list_name: 'Vendor',
-      entities: 0,
+      date: 'Jul 28',
+      list_name: 'To_index',
+      entities: 100,
       description: [
         'company 1', 'company 1', 'company 1', 'company 1', 'company 1'
       ]
      },
      {
-      date: 'Jul 22',
-      list_name: 'Vendor',
-      entities: 0,
+      date: 'Apr 10',
+      list_name: 'KPMG Requested Companies - Listed Set_Prasad Kumar',
+      entities: 34,
       description: [
         'company 1', 'company 1', 'company 1', 'company 1', 'company 1'
       ]
      },
      {
-      date: 'Jul 22',
-      list_name: 'Vendor',
-      entities: 0,
+      date: 'Mar 12',
+      list_name: 'Error Case - LTB To EBITDA Blank_Prasad Kumar',
+      entities: 24,
       description: [
         'company 1', 'company 1', 'company 1', 'company 1', 'company 1'
       ]
      },
      {
-      date: 'Jul 22',
-      list_name: 'Vendor',
-      entities: 0,
+      date: 'Dec 14',
+      list_name: 'Two Companies',
+      entities: 2,
       description: [
         'company 1', 'company 1', 'company 1', 'company 1', 'company 1'
       ]
      },
      {
-      date: 'Jul 22',
-      list_name: 'Vendor',
-      entities: 1,
+      date: 'Nov 01',
+      list_name: 'Custom',
+      entities: 30,
       description: [
         'company 1', 'company 1', 'company 1', 'company 1', 'company 1'
       ]
      },
      {
-      date: 'Jul 22',
-      list_name: 'Vendor',
-      entities: 22,
+      date: 'Oct 29',
+      list_name: 'Mumbai',
+      entities: 3,
       description: [
         'company 1', 'company 1', 'company 1', 'company 1', 'company 1'
       ]
      },
      {
-      date: 'Jul 22',
+      date: '',
       list_name: 'Vendor',
       entities: 0,
       description: [
